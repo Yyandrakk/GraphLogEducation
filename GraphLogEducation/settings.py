8a96445ba5f37dd05b9d3041788b43acafbc217b
Yyandrakk <oscargdlp@gmail.com>
2017-12-29 16:50:33 +0100

    Cambiada a postgresql y añadido bootstrap

--- GraphLogEducation/settings.py
+++ GraphLogEducation/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/2.0/ref/settings/
 
 import os
 
+import local_settings
+
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -38,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'GraphLog.apps.GraphlogConfig',
+    'usuarios',
 ]
 
 MIDDLEWARE = [
@@ -77,8 +80,12 @@ WSGI_APPLICATION = 'GraphLogEducation.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'graphLogDB',
+        'USER': local_settings.DB_USER,
+        'PASSWORD': local_settings.DB_PASS,
+        'HOST': 'localhost',
+        'PORT': '',   
     }
 }
 
