0c3d88b50ce1c7c6bcddab5bda10d88e421d1a02
Yyandrakk <oscargdlp@gmail.com>
2018-01-07 19:12:01 +0100

    fix: Cambios esteticos

--- usuarios/forms.py
+++ usuarios/forms.py
@@ -8,6 +8,13 @@ class FormLogin(forms.ModelForm):
         model = Usuario
         fields = ['username','password']
 
+    def __init__(self,*args,**kargs):
+        super(FormLogin,self).__init__(*args,**kargs)
+        self.fields['password'].widget = forms.PasswordInput(attrs={'class': 'form-control'})
+        for v in self.visible_fields():
+            v.field.widget.attrs['class'] = 'form-control'
+
+
 
 class FormRegister(forms.ModelForm):
 
@@ -16,3 +23,20 @@ class FormRegister(forms.ModelForm):
     class Meta():
         model = Usuario
         fields = ('username','email','password','first_name','last_name')
+
+    def __init__(self,*args,**kargs):
+        super(FormRegister,self).__init__(*args,**kargs)
+        self.fields['password'].widget = forms.PasswordInput(attrs={'class': 'form-control'})
+        for v in self.visible_fields():
+            v.field.widget.attrs['class'] = 'form-control'
+
+    def clean(self):
+        datos = super(FormRegister,self).clean()
+        passw1 = datos.get("password")
+        passw2 = datos.get("val_password")
+
+        if passw1 != passw2:
+            raise forms.ValidationError("Las contraseñas no coinciden")
+
+        return datos
+
