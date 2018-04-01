45da3c314b68f6f7659b447d674ae2e46c81fa73
oscar <oscargdlp@gmail.com>
2018-04-01 19:56:22 +0200

    corrigiendo settings

--- GraphLog/urls.py
+++ GraphLog/urls.py
@@ -7,4 +7,8 @@ app_name = 'graph'
 
 urlpatterns = [
   path('',indexView.as_view(),name='index')
-] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
